4f8130d8219303d837bd8ed320ae9f0317c72ca9
Dzakwan Diego <diegodzakwan@gmail.com>
2023-11-27 13:22:54 +0700

    [Update] env: change API address for wilayah

--- src/stores/envStore.js
+++ src/stores/envStore.js
@@ -4,6 +4,6 @@ import { defineStore } from "pinia";
 export const useEnvStore = defineStore("env", {
   state: () => ({
     apiUrl: "http://178.16.143.152:3000/",
-    addressUrl: "https://yusufva.github.io/api-wilayah-indonesia/api/",
+    addressUrl: "https://dzakwandp.github.io/api-wilayah-indonesia/api/",
   }),
 });
